51dcb0698120938ba782403f8d5eebb50a2f9914
Vaibhav Vemani <vaibhavvemani@gmail.com>
2025-02-18 16:46:32 +0530

    saving sam

--- src/components/server/student_retrieval.tsx
+++ src/components/server/student_retrieval.tsx
@@ -4,6 +4,7 @@ const url = 'https://hrcmqezcdkbmxgdfxxqo.supabase.co';
 const anon_key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhyY21xZXpjZGtibXhnZGZ4eHFvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk4NTcwMTUsImV4cCI6MjA1NTQzMzAxNX0.vylGgJ5oJadZfTT5w_c816EIPCtp1Zpppe2PSu42qTw';
 
 const supabase = createClient(url, anon_key);
+// Hello
 
 const get_student_details = async (usn: string) => {
   const { data, error } = await supabase
