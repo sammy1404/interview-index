67d484c036506ce34b739461b888b2c4fa1bd68a
Vaibhav Vemani <vaibhavvemani@gmail.com>
2025-02-18 15:44:58 +0530

    data access added

--- src/components/ui/student_display.tsx
+++ src/components/ui/student_display.tsx
@@ -2,30 +2,25 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 import { get_student_details } from '../server/student_retrieval'
-import { useEffect, useState} from 'react'
+import { useState } from 'react'
 
 export default function Student_display() {
 
   const [students, setStudents] = useState<any[]>([]);
   const [usn, setUSN] = useState("");
-  const [name, setName] = useState("");
-
+  
   const get_details = async () => {
     const data = await get_student_details(usn);
     if (data) setStudents(data);
   };
 
-  useEffect(() => {
-    if (students.length > 0) {
-      setName(students[0].Name);
-    }
-  }, [students])
 
 
   return (
     <div>
       <div className="student-info-container">
-        <p>{name}</p>
+        <p>{students[0]?.Name}</p>
+        <p>{students[0]?.USN}</p>
       </div>
       <input type="text" onChange={(e) => setUSN(e.target.value)} />
       <button onClick={get_details}>Load Student</button>
