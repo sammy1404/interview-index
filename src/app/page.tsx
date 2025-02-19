8c0d373916e2ef95c172af3355068db4f129c179
sammy144 <samreeves1404@gmail.com>
2025-02-19 15:51:23 +0530

    something

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useAuth, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import Clerk from "@/components/ui/Clerk";
 import { useAuth } from '@clerk/clerk-react';
 
