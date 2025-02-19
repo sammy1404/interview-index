bde45ce33ab67ad44d40da933797c30653249ae8
sammy144 <samreeves1404@gmail.com>
2025-02-19 11:59:23 +0530

    shadcn theme changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,8 @@ const page = () => {
   }, [isSignedIn]);
 
   return (
-    <div>
+    <div className='flex flex-col items-center justify-center h-screen'>
+      <h1 className='text-4xl md:text-8xl font-bold text-center mb-8 bg-gradient-to-r from-blue-500 to-purple-500 text-transparent bg-clip-text'>Interview Index</h1>
       <Clerk />
     </div>
   )
