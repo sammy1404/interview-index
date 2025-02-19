501976b1a851d2999cb0d6cfc4c85bba9b824648
sammy144 <samreeves1404@gmail.com>
2025-02-19 11:22:59 +0530

    page wrapped in admin route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,23 @@
 "use client"
-import "../components/styles/page.css"
 
-import Student_display from '../components/creativitiy/student_display'
-import Interview_display from '@/components/creativitiy/interview_display';
+import React, { use, useEffect } from 'react'
 import Clerk from "@/components/ui/Clerk";
+import { useAuth } from '@clerk/clerk-react';
 
-import { useState } from 'react'
+const page = () => {
+  const { isSignedIn } = useAuth();
 
-export default function Home() {
-  const [usn, setUSN] = useState("");
+  useEffect(() => {
+    if (isSignedIn) {
+      window.location.href = '/admin';
+    }
+  }, [isSignedIn]);
 
   return (
-    <div className='pl-5 pr-5 pt-5 flex flex-col gap-5'>
-      <header className='header'>
-        <h1>Interview Index</h1>
-        <nav className='nav'>
-          <ul className='nav-links'>
-            <li className='links'>Student Details</li>
-            <li className='links'>Company Entry</li>
-            <li className='links'>Student Entry</li>
-            <Clerk />
-          </ul>
-        </nav>
-      </header>
-      
-      <input onChange={(e) => setUSN(e.target.value)} placeholder='Example: 1NH22AI185' className="usn-input"></input>
-
-      <div className="flex flex-col md:flex-row gap-6 md:gap-10 justify-center items-center">
-        <div className="w-full md:w-[350px] h-[50vh] md:h-[80vh] rounded-lg" style={{ backgroundColor: 'var(--primary)'}}>
-          <Student_display usn={ usn }/>
-        </div>
-
-        <div className="flex flex-col w-full md:w-[650px] h-auto md:h-[80vh] gap-6 md:gap-10 p-5 border-4 rounded-lg" style={{ borderColor: 'var(--primary)' }}>
-          <Interview_display usn={ usn }/>
-        </div>
-      </div>
+    <div>
+      <Clerk />
     </div>
-  );
+  )
 }
+
+export default page
