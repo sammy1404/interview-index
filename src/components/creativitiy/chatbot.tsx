192d227c72519556f910dd5123c9796ff6ca4722
Vaibhav Vemani <vaibhavvemani@gmail.com>
2025-02-24 09:34:27 +0530

    ChatBot component started

--- src/components/creativitiy/chatbot.tsx
+++ src/components/creativitiy/chatbot.tsx
@@ -2,36 +2,18 @@ import React from 'react'
 import "../styles/chatbot.css"
 
 import { Button } from "@/components/ui/button"
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet"
-
+import { useState } from "react"
 
 
 export default function Chatbot() {
 
+  const [isOpen, setOpen] = useState(false)
+
+
   return (
     <div className='chatbot-container'>
-       <Sheet>
-        <SheetTrigger>
-          <Button>Open</Button>
-        </SheetTrigger>
-        <SheetContent>
-          <SheetHeader>
-            <SheetTitle>Chat with me and find out more about yourself</SheetTitle>
-            <SheetDescription>
-              This action cannot be undone. This will permanently delete your account
-              and remove your data from our servers.
-            </SheetDescription>
-          </SheetHeader>
-        </SheetContent>
-      </Sheet>
-   </div>
+      <Button variant="default">Open Chat</Button>
+    </div>
   )
 }
 
